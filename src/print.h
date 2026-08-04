ea0e8bdefb534cf2812170dfd5e8f5ecd596700e
osami <v.pylayev@mail.ru>
2026-08-04 09:20:40 +0500

    add print.h

---
+++ src/print.h
@@ -0,0 +1,5 @@
+#include "raylib.h"
+
+void print_image_info(long size, int w, int h, int channels);
+void print_cursor_cords(int width, int height, Texture texture,
+                        Camera2D camera);
